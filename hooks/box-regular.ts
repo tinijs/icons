06757a2ceb4525a2dd8737355eb0a44a8fdc2db5
Lam Nhan <nhan.lh@raksul.com>
2024-03-01 21:13:06 +0700

    Combine variants

--- hooks/box-regular.ts
+++
@@ -1,9 +0,0 @@
-export default function () {
-  const transformName = function (name: string) {
-    return name.replace(/bx-/g, '');
-  };
-
-  return {
-    transformName,
-  };
-}
--- hooks/box.ts
+++ hooks/box.ts
@@ -1,6 +1,6 @@
 export default function () {
   const transformName = function (name: string) {
-    return name.replace(/bxs-/g, '');
+    return name.replace(/(bx|bxl|bxs)-/g, '');
   };
 
   return {
